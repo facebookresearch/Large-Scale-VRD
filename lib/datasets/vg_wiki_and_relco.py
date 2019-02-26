c0df194276ce410ec87c342f1bc390328c82c02f
Ji Zhang <zhangjiapply@gmail.com>
2019-02-26 17:15:05 -0500

    Update vg_wiki_and_relco.py

--- lib/datasets/vg_wiki_and_relco.py
+++ lib/datasets/vg_wiki_and_relco.py
@@ -142,7 +142,7 @@ class vg_wiki_and_relco(imdb_rel):
         self.relco_model = gensim.models.Word2Vec.load(
             cfg.DATA_DIR + '/label_embeddings/vg_300d_skipgram_rel')
         print('Model loaded.')
-        self.relco_vec_mean = self.relco_model.syn0.mean(axis=0)
+        self.relco_vec_mean = self.relco_model.wv.syn0.mean(axis=0)
         # change everything into lowercase
         # for key in self.model.wv.vocab.keys():
         for key in self.relco_model.vocab.keys():
